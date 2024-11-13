2e59e1055419c726a462e25ba78b2fefe18dfe18
Prince Tiwari <princetiwari.profes@gmail.com>
2024-11-13 20:13:03 +0530

    Index file

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const cors = require('cors')
+const env = require('dotenv')
+env.config()
 
 const app = express();
+const PORT = process.env.PORT || 8000
 
-app.listen()
+app.listen(PORT, ()=>{
+    console.log(`Server is running on Port ${PORT}`)
+})
